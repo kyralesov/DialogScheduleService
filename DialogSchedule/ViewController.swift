fa7cfa0f54326c2ef61294b19a8ae3f52242ce84
Eugene Kurilenko <kyralesov@gmail.com>
2017-01-23 10:13:06 +0200

    Initial Commit

---
+++ DialogSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DialogSchedule
+//
+//  Created by Eugene Kurilenko on 1/23/17.
+//  Copyright © 2017 Eugene Kurilenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
